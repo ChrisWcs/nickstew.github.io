77f4eb154b085ddf516f74ce4264a9bfcd40dbba
Christian <ChrisWcs@users.noreply.github.com>
2017-08-20 12:17:29 -0600

    Begin transferring resume over

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
           <div className="row">
             <div className="primary col-md-8 col-sm-12 col-xs-12">
               <About />
-              <MainProjects />
+              <MainProjects/>
               <OtherProjects />
               <WorkExperience />
               <Github />
