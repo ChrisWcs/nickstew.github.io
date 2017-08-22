c5ce563cf8e9684aa55cef49d725f4b618f1b579
Christian <ChrisWcs@users.noreply.github.com>
2017-08-22 12:57:58 -0600

    Add to Projects, create presentation component called Project

--- src/Projects.js
+++ src/Projects.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import resume from './resume.json';
 
+const IndividualProj = ({ name, summary }) => (
+  <div className="content">
+    <h4>{name}</h4>
+    <p>{summary}</p>
+  </div>
+);
+
 const Projects = () => (
   <section className="projects section">
     <div className="section-inner">
       <h2 className="heading">Projects</h2>
       <div className="content">
+        {resume.publications.map( publication => <IndividualProj {...publication} />  )}
         <a className="btn btn-cta-secondary">
           More on GitHub <i className="fa fa-chevron-right"></i>
         </a>
